738ab4bffb92ee171c4dc8ebd97a8296a3302554
Heitor Camara <heitor.eumesmo@gmail.com>
2018-12-22 09:48:47 -0300

    improved code

---
+++ basics/boolean.js
@@ -0,0 +1,44 @@
+let isImportant = true;
+console.log('Resultado: ' + isImportant);
+
+let temp = 34;
+let isFreezing = temp <= 28;
+console.log('Está frio? ' + isFreezing);
+
+let firstName = 'Heitor';
+let lastName = 'Camara';
+
+let result01 = firstName === lastName; // false
+let result02 = firstName !== lastName; // true
+
+let word = 'SHOW'
+let word02 = 'show'
+console.log('Resultado Novo: ' + (word === word02));
+
+console.log('Resultado 1:' + result01);
+console.log('Resultado 2:' + result02);
+
+let num01 = 10;
+let num02 = 20;
+
+let result03 = num01 < num02;
+let result04 = num02 > num01;
+
+console.log('Resultado 3:' + result03);
+console.log('Resultado 4:' + result04);
+
+
+let val01 = 100;
+let val02 = 200;
+
+let result05 = val01 <= val02;
+let result06 = val02 >= val01;
+
+console.log('Resultado 5: ' + result05);
+console.log('Resultado 6: ' + result06);
+
+let word03 = 'cadeira';
+let num03 = 100;
+
+newResult = word03 === num03;
+console.log(newResult);
