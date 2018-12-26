2c4695e91114e357737a196057f572994cc2b616
Heitor Camara <heitor.eumesmo@gmail.com>
2018-12-26 18:41:40 -0300

    challenge related to template strings

---
+++ functions/challenge-template-strings.js
@@ -0,0 +1,19 @@
+let getTipTwo = function (total, tipPercent = .2) { //can be used 0.2 or .2
+    let percent = tipPercent * 100;
+    let tip = total * tipPercent;
+
+    return `A ${percent}% tip on R$${total} would be R$${tip}`;
+};
+
+let getAverageScore = function (resultOne, resultTwo) {
+    return `The average score is ${(resultOne + resultTwo)/2}`
+};
+
+let resultOne = getTipTwo(200, .3);
+console.log(resultOne);
+
+let resultTwo = getTipTwo(2450, .5);
+console.log(resultTwo);
+
+let result = getAverageScore(7.5, 9);
+console.log(result);
