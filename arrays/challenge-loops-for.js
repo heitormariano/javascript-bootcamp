ec686a1a6dbd24ce1bc8fd684e637cbb92cfb9c2
Heitor Camara <heitor.eumesmo@gmail.com>
2019-07-08 20:52:12 -0300

    variable changed

--- arrays/challenge-loops-for.js
+++ arrays/challenge-loops-for.js
@@ -1,12 +1,12 @@
-let todoOne, todoTwo, todoThree, todoFour, todofive;
+let todoOne, todoTwo, todoThree, todoFour, todoFive;
 
 todoOne = 'More time with God';
 todoTwo = 'Learn English';
 todoThree = 'Get back to physical activities';
 todoFour = 'Rest a little bit during week';
-todofive = 'Learn Programming';
+todoFive = 'Learn Programming';
 
-const todos = [todoOne, todoTwo, todoThree, todoFour, todofive];
+const todos = [todoOne, todoTwo, todoThree, todoFour, todoFive];
 
 for (let count = 0; count < todos.length; count++) {
     const num = count + 1;
