65c8c24db9e15cc2aa6675813cc327751c55cf4e
Heitor Camara <heitor.eumesmo@gmail.com>
2019-07-18 11:43:58 -0300

    code improved

--- js-browser/lesson-053/js/event-test-v2.js
+++ js-browser/lesson-053/js/event-test-v2.js
@@ -1,10 +1,18 @@
 const modifyUserName = function (idElem, value) {
     if (typeof (idElem) === 'string' && typeof (value) === 'string') {
         let elem = document.getElementById(idElem);
-        if (elem.firstChild.nodeValue == 'Tompson') {
-            elem.firstChild.nodeValue = value;
-        } else {
-            elem.firstChild.nodeValue = 'Tompson';
+        if (idElem == 'user-01') {
+            if (elem.firstChild.nodeValue == 'Tompson') {
+                elem.firstChild.nodeValue = value;
+            } else {
+                elem.firstChild.nodeValue = 'Tompson';
+            }
+        } else if (idElem == 'user-02') {
+            if (elem.firstChild.nodeValue == 'Richard') {
+                elem.firstChild.nodeValue = value;
+            } else {
+                elem.firstChild.nodeValue = 'Richard';
+            }
         }
     }
 };
